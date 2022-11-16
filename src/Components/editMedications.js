176451dd81dfb9ccf8766c785d01d70322ed0c44
rbidon <roussebidon@gmail.com>
2022-11-15 22:13:13 -0500

    Fixing heroku issue

--- src/Components/editMedications.js
+++ src/Components/editMedications.js
@@ -3,7 +3,15 @@ import React, {useState} from 'react'
 // Import Fontawesome for styling for the close button
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { solid } from '@fortawesome/fontawesome-svg-core/import.macro'
-let baseURL= process.env.REACT_APP_BASE_URL
+
+let baseURL = ''
+if(process.env.NODE_ENV === 'development'){
+  baseURL = process.env.REACT_APP_BASE_URL
+} else{
+  // heroku backend url
+  baseURL = process.env.REACT_APP_API_URL
+}
+// let baseURL= process.env.REACT_APP_BASE_URL
 
 const EditMedications = ({data,setEditOpenModal,editMedications})=>{
     // {medicationsToEdit,editMedications,setOpenModal}
